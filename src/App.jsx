e4acfecdf7d87e1edd28ab6a1fc530dead815456
Maksym S <m.symonenko@kyrrex.com>
2025-01-09 21:06:12 +0200

    add Telegram login widget integration in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src = "https://telegram.org/js/telegram-widget.js?22"
+    script.async = true
+    script.setAttribute('data-telegram-login', 'demo_test_1212_bot')
+    script.setAttribute('data-size', 'small')
+    script.setAttribute('data-userpic', 'false')
+    script.setAttribute('data-radius', '10')
+    script.setAttribute('data-onauth', 'onTelegramAuth(user)')
+    script.setAttribute('data-request-access', 'write')
+    document.getElementById('telegram-login').appendChild(script)
+  }, [])
+
+  window.onTelegramAuth = function(user) {
+    alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')')
+  }
 
   return (
     <>
@@ -28,6 +45,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div id="telegram-login"></div>
     </>
   )
 }
